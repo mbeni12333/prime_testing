ada21da68445a1ae243c27bed35a8cd82b51f621
Guillaume Magniadas <guillaume.magniadas@gmail.com>
2020-11-23 12:26:18 +0100

    Ajout de my_expo_mod et de ses tests.

--- src/fonctions.py
+++ src/fonctions.py
@@ -12,3 +12,21 @@ def my_gcd(a, b):
 		a = tempo
 
 	return a
+
+def my_expo_mod(g, n, N):
+	"""
+	int*int*int -> int
+	retourn (g^n) % N
+	"""
+	h = 1
+
+	l = n.bit_length()
+
+	#Note: on met la range jusqu'a -1 pour que i prenne aussi la valeur 0.
+	for i in range(l - 1, -1, -1):
+		h = (h**2) % N
+
+		if (n >> i) & 1 == 1:
+			h = (h * g) % N
+
+	return h
--- src/test.py
+++ src/test.py
@@ -1,7 +1,21 @@
 
-from fonctions import my_gcd
+from fonctions import my_gcd, my_expo_mod
 
 """
 Le pgcd de 987 et 345 est 3 :
 """
 print("Pgcd de 987 et 345:", my_gcd(987, 345))
+
+
+"""
+2^10 modulo 67 vaut 19
+"""
+
+print("\n2^10 modulo 67:")
+print("Avec pow de python:", pow(2, 10, mod=67))
+print("Avec my_expo_mod:", my_expo_mod(2, 10, 67))
+
+
+print("\n7892^678 modulo 79:")
+print("Avec pow de python:", pow(7892, 678, mod=79))
+print("Avec my_expo_mod:", my_expo_mod(7892, 678, 79))
